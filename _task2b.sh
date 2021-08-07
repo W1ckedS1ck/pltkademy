c9d37afdd7eba632c96368ca90dba9b49dd09d7c
W1ckedS1ck <61629889+W1ckedS1ck@users.noreply.github.com>
2021-08-07 17:27:13 +0300

    _task2b.sh has been updated
    But this file has been released later than planned. (:

--- _task2b.sh
+++ _task2b.sh
@@ -4,7 +4,7 @@ cd 2
 num=0
 while [ $num -lt 10 ]
 do
-rd_=$(cat /dev/urandom | tr -cd 'a-z' | head -c 5)
-base64 < /dev/urandom | head -c 1000 > "$rd_$num".txt
+rd=$(cat /dev/urandom | tr -cd 'a-z' | head -c 5)
+base64 < /dev/urandom | head -c 1000 > "$rd _$num".txt
 num=$(( $num + 1 ))
 done
